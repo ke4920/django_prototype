78c3913b85ed8ceca94e69fbaeddcc71c6c613e4
Jonas Dressner <jonas.dressner@kit.edu>
2023-10-11 17:08:49 +0200

    added navbar

---
+++ scripts/data_download.sh
@@ -0,0 +1,1 @@
+curl 'http://localhost:8080/api/v1/dataresources/677a9242-9fc4-4292-861f-cb5fbb941c9a/data/randomFile.txt' -i -X GET
---
+++ scripts/get_dataresource.sh
@@ -0,0 +1,1 @@
+curl 'http://localhost:8080/api/v1/dataresources/478c6e2b-0929-4bf2-81c5-6824325c52ca' -i -X GET
