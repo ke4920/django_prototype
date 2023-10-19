22a862ed80ab100f17e53327664d0de5025bad55
Jonas Dressner <jonas.dressner@kit.edu>
2023-10-19 11:50:46 +0200

    added first test

--- test/test_database.py
+++ test/test_database.py
@@ -1,3 +1,20 @@
 import unittest
 
-import assasdb import DatabaseManager
+from assasdb import DatabaseManager
+
+class DatabaseManagerTest(unittest.TestCase):
+    
+    def setUp(self):
+        
+        self.database_manager = DatabaseManager()
+        
+    def tearDown(self):
+        
+        self.database_manager = None
+
+    def test_database_manager_basic(self):
+
+        self.database_manager.upload()
+        
+if __name__ == '__main__':
+    unittest.main()
