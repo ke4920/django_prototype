0b473eef16022a8a80fc74f336f7b7aa3b0d8556
Jonas Dressner <jonas.dressner@kit.edu>
2024-02-01 18:49:13 +0100

    added first state with dash datatable

--- dash/app.py
+++ dash/app.py
@@ -1,37 +1,61 @@
-from dash import Dash, dash_table, Input, Output, callback
+from dash import Dash, dash_table, dcc, html, Input, Output, callback
 import pandas as pd
-import numpy as np
-from collections import OrderedDict
 
-from assasdb import AssasDatabaseManager
 
-df = AssasDatabaseManager().view()
-df['index'] = range(1, len(df) + 1)
-df = df.drop("_id",axis=1)
-df = df.drop("path",axis=1)
-df = df.drop("uuid",axis=1)
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
-print(df, type(df))
+df[' index'] = range(1, len(df) + 1)
 
 app = Dash(__name__)
 
-PAGE_SIZE = 1
+PAGE_SIZE = 5
 
-app.layout = dash_table.DataTable(
-    data=df.to_dict('records'),
-    columns=[{'id': c, 'name': c} for c in df.columns],
-    page_size=20
-)
+app.layout = html.Div([
+    dash_table.DataTable(
+        id='datatable-paging-page-count',
+        columns=[
+            {"name": i, "id": i} for i in sorted(df.columns)
+        ],
+        page_current=0,
+        page_size=PAGE_SIZE,
+        page_action='custom'
+    ),
+    html.Br(),
+    dcc.Checklist(
+        id='datatable-use-page-count',
+        options=[
+            {'label': 'Use page_count', 'value': 'True'}
+        ],
+        value=['True']
+    ),
+    'Page count: ',
+    dcc.Input(
+        id='datatable-page-count',
+        type='number',
+        min=1,
+        max=29,
+        value=20
+    )
+])
 
-#@callback(
-#    Output('datatable-paging', 'data'),
-#    Input('datatable-paging', "page_current"),
-#    Input('datatable-paging', "page_size"))
-#def update_table(page_current,page_size):
-#    return df2.iloc[
-#        page_current*page_size:(page_current+ 1)*page_size
-#    ].to_dict('records')
 
+@callback(
+    Output('datatable-paging-page-count', 'data'),
+    Input('datatable-paging-page-count', "page_current"),
+    Input('datatable-paging-page-count', "page_size"))
+def update_table(page_current,page_size):
+    return df.iloc[
+        page_current*page_size:(page_current+ 1)*page_size
+    ].to_dict('records')
+
+@callback(
+    Output('datatable-paging-page-count', 'page_count'),
+    Input('datatable-use-page-count', 'value'),
+    Input('datatable-page-count', 'value'))
+def update_table(use_page_count, page_count_value):
+    if len(use_page_count) == 0 or page_count_value is None:
+        return None
+    return page_count_value
 
 if __name__ == '__main__':
     app.run(debug=True)
--- dash/dataview.py
+++ dash/dataview.py
@@ -1,81 +1,241 @@
-from dash import Dash, dash_table, html, dcc, Input, Output, callback
+import dash
+import dash_bootstrap_components as dbc
+from dash import Dash, dash_table, html, dcc, Input, Output, callback, State
 import pandas as pd
 import numpy as np
 from collections import OrderedDict
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 from assasdb import AssasDatabaseManager
 
-df = AssasDatabaseManager().view()
-df['index'] = range(1, len(df) + 1)
-df = df.drop("_id",axis=1)
-df = df.drop("path",axis=1)
-df = df.drop("uuid",axis=1)
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
 
-print(df, type(df))
+def load_data():
+    
+    df = AssasDatabaseManager().view()
+    df['index'] = range(1, len(df) + 1)
+    df = df.drop('_id',axis=1)
+    df = df.drop('file_path',axis=1)
+    df = df.drop('uuid',axis=1)
+    #df = df.drop('common_description',axis=1)
+    
+    logger.info(df, type(df))
+    
+    return df
+    
+df = load_data()
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-PAGE_SIZE = 10
+ALL = len(df)
+PAGE_SIZE = 30
+PAGE_MAX_SIZE = 100
+
+PAGE_COUNT = ALL / PAGE_SIZE
 
 app.layout = html.Div([
+    html.H2('ASSAS Database - ASTEC Dataset Index'),
+    dbc.Alert("This is a primary alert", color="primary"),
+    dbc.Pagination(
+                id='pagination', 
+                first_last=True,
+                previous_next=True,
+                max_value=PAGE_COUNT, 
+                fully_expanded=False,
+                size="lg"
+                ),
     dash_table.DataTable(
         id='datatable-paging-and-sorting',
         columns=[
-            {'name': i, 'id': i, 'deletable': True} for i in sorted(df.columns)
+        {'name': ['', 'Index'], 'id': 'index'},
+        {'name': ['File', 'Name'], 'id': 'file_name'},
+        {'name': ['File', 'Size'], 'id': 'file_size'},
+        {'name': ['File', 'Date'], 'id': 'file_date'},
+        {'name': ['File', 'User'], 'id': 'file_user'},
+        {'name': ['File', 'Download'], 'id': 'file_download'},
+        {'name': ['Common', 'Scenario'], 'id': 'common_scenario'},
+        {'name': ['Common', 'Description'], 'id': 'common_description'},
+        {'name': ['Common', 'Attribute 1'], 'id': 'common_attribute_1'},
+        {'name': ['Common', 'Attribute 2'], 'id': 'common_attribute_2'},
+        {'name': ['Common', 'Attribute 3'], 'id': 'common_attribute_3'},
+        {'name': ['Data', 'Variables'], 'id': 'data_variables'},
+        {'name': ['Data', 'Channels'], 'id': 'data_channels'},
+        {'name': ['Data', 'Meshes'], 'id': 'data_meshes'},
+        {'name': ['Data', 'Timesteps'], 'id': 'data_timesteps'},
         ],
+        data=df.to_dict('records'),
+        style_cell = {'textAlign': 'center'},
+        merge_duplicate_headers= True,        
+        
+        style_header={
+            'backgroundColor': 'rgb(30, 30, 30)',
+            'color': 'white'
+        },
+        style_data={
+            'backgroundColor': 'rgb(50, 50, 50)',
+            'color': 'white'
+        },
+        
         page_current=0,
         page_size=PAGE_SIZE,
         page_action='custom',
+                
+        filter_action='custom',
+        filter_query='',
 
         sort_action='custom',
-        sort_mode='single',
-        sort_by=[]
+        sort_mode='multi',
+        sort_by=[],
+        
+        is_focused=True,
+        style_data_conditional=[
+                {'if': {'column_id': 'index'}, 'backgroundColor': 'green', 'text_align':'center','color': 'white'},
+                {'if': {'column_id': 'file_download'}, 'backgroundColor': 'yellow', 'color': 'red', 'font-weight': 'bold'},
+        ],        
     ),
+    dcc.Location(id='location'),
+    dcc.Download(id='download'),
     html.Br(),
-    #dcc.Checklist(
-    #    id='datatable-use-page-count',
-    #    options=[
-    #        {'label': 'Use page_count', 'value': 'True'}
-    #    ],
-    #    value=['True']
-    #),
-    'Page count: ',
+    dcc.Checklist(
+        id='datatable-use-page-size',
+        options=[
+            {'label': 'Change entries per page', 'value': 'True'}
+        ],
+        value=['True']
+    ),
+    'Entries per page: ',
     dcc.Input(
-        id='datatable-page-count',
+        id='datatable-page-size',
         type='number',
         min=1,
-        max=29,
-        value=10
-    )
+        max=PAGE_MAX_SIZE,
+        value=PAGE_SIZE
+    ),
+    html.Div('Select a page', id='pagination-contents'),                 
+    #html.Div('Or set the page dynamically using the slider below'),
+    #dcc.Slider(
+    #    id='page-change',
+    #    min=1,
+    #    max=len(df) + 1,
+    #    step=1,
+    #    value=1,
+    #    marks={i: str(i) for i in range(1, 11)},
+    #),
 ])
 
-@callback(
-    Output('datatable-paging-and-sorting', 'page_count'),
-    Input('datatable-page-count', 'value'))
-def update_table(page_count_value):
-    if page_count_value is None:
-        return None
-    return page_count_value
+operators = [['ge ', '>='],
+             ['le ', '<='],
+             ['lt ', '<'],
+             ['gt ', '>'],
+             ['ne ', '!='],
+             ['eq ', '='],
+             ['contains '],
+             ['datestartswith ']]
+
+def split_filter_part(filter_part):
+    for operator_type in operators:
+        for operator in operator_type:
+            if operator in filter_part:
+                name_part, value_part = filter_part.split(operator, 1)
+                name = name_part[name_part.find('{') + 1: name_part.rfind('}')]
+
+                value_part = value_part.strip()
+                v0 = value_part[0]
+                if (v0 == value_part[-1] and v0 in (''', ''', '`')):
+                    value = value_part[1: -1].replace('\\' + v0, v0)
+                else:
+                    try:
+                        value = float(value_part)
+                    except ValueError:
+                        value = value_part
+
+                # word operators need spaces after them in the filter string,
+                # but we don't want these later
+                return name, operator_type[0].strip(), value
+
+    return [None] * 3
 
 @callback(
     Output('datatable-paging-and-sorting', 'data'),
     Input('datatable-paging-and-sorting', 'page_current'),
     Input('datatable-paging-and-sorting', 'page_size'),
-    Input('datatable-paging-and-sorting', 'sort_by'))
-def update_table(page_current, page_size, sort_by):
+    Input('datatable-paging-and-sorting', 'sort_by'),
+    Input('datatable-paging-and-sorting', 'filter_query'))
+def update_table(page_current, page_size, sort_by, filter):
+    filtering_expressions = filter.split(' && ')
+    dff = df
+    for filter_part in filtering_expressions:
+        col_name, operator, filter_value = split_filter_part(filter_part)
+
+        if operator in ('eq', 'ne', 'lt', 'le', 'gt', 'ge'):
+            # these operators match pandas series operator method names
+            dff = dff.loc[getattr(dff[col_name], operator)(filter_value)]
+        elif operator == 'contains':
+            dff = dff.loc[dff[col_name].str.contains(filter_value)]
+        elif operator == 'datestartswith':
+            # this is a simplification of the front-end filtering logic,
+            # only works with complete fields in standard format
+            dff = dff.loc[dff[col_name].str.startswith(filter_value)]
+
     if len(sort_by):
-        dff = df.sort_values(
-            sort_by[0]['column_id'],
-            ascending=sort_by[0]['direction'] == 'asc',
+        dff = dff.sort_values(
+            [col['column_id'] for col in sort_by],
+            ascending=[
+                col['direction'] == 'asc'
+                for col in sort_by
+            ],
             inplace=False
         )
-    else:
-        # No sort is applied
-        dff = df
 
-    return dff.iloc[
-        page_current*page_size:(page_current+ 1)*page_size
-    ].to_dict('records')
+    page = page_current
+    size = page_size
+    return dff.iloc[page * size: (page + 1) * size].to_dict('records')
+
+@callback(
+    Output('datatable-paging-and-sorting', 'page_size'),
+    Input('datatable-use-page-size', 'value'),
+    Input('datatable-page-size', 'value'))
+def update_page_size(use_page_size, page_size_value):
+    if len(use_page_size) == 0 or page_size_value is None:
+        return PAGE_SIZE
+    return page_size_value
+
+@callback(
+    Output('pagination-contents', 'children'),
+    [Input('pagination', 'active_page')])
+def change_page(page):
+    if page:
+        return f'Page selected: {page}'
+    return 'Select a page'
+
+@callback(
+    Output('datatable-paging-and-sorting', 'page_current'),
+    [Input('pagination', 'active_page')])
+def change_page_table(page):
+    if page:
+        return page-1
+    return 0
+
+@callback(
+    Output('download', 'data'),
+    Input('datatable-paging-and-sorting', 'active_cell'),
+    State('datatable-paging-and-sorting', 'derived_viewport_data'))
+def cell_clicked(active_cell, data):
+    if active_cell:
+        row = active_cell['row']
+        col = active_cell['column_id']
+
+        if col == 'index':  # or whatever column you want
+            selected = data[row][col]
+            return dict(content='Hello!', filename='hello.txt')
+        else:
+            return dash.no_update
 
 if __name__ == '__main__':
     app.run(debug=True, port=8051)
